6cd04898eaa0242835b074947bb0032088e63980
Juandiego Rodriguez <jdrod0803@gmail.com>
2020-03-28 14:23:55 -0400

    Started development on quotes endpoint, added json, htmls for testing

--- CBSD_Project_Flask/forms.py
+++ CBSD_Project_Flask/forms.py
@@ -2,49 +2,28 @@ from CBSD_Project_Flask import main_functions
 import requests
 from flask_wtf import FlaskForm
 from wtforms import SelectField
+from wtforms import StringField
 
-class AQIParameters(FlaskForm):
-    #aqiparameter = SelectField('aqiparameter', choices = ["temperatureC","pressure","humidity","aqius"])
-    aqiparameter = SelectField('aqiparameter', choices = [("temperatureC","TemperatureC"),
-                                                          ("pressure","Pressure"),
-                                                          ("humidity","Humidity"),
-                                                          ("aqius","Aqius")])
+class SymbolSearchForm(FlaskForm):
+    symbol = StringField(u"Enter stock symbol")
+
     '''
     Complete this class with the appropriate parameters inside SelectField for the dropdown menu
     '''
+def retrieve_quote_parameters(symbol):
+    #Concatenate stuff to send out the request.
+    base_url = "https://cloud.iexapis.com/stable/stock/" + symbol + "/quote"
+    iex_key = main_functions.read_from_file("CBSD_Project_Flask/JSON_Files/iex_key.json")["iex_key"]
+    request_url = base_url + "?token=" + iex_key
+    #JSON Stuff
+    response_json = requests.get(request_url).json() #Get the json based on the URL
+    main_functions.save_to_file(response_json, "CBSD_Project_Flask/JSON_Files/retrieve_quote.json") #Save the json to file
+    response_json_file = main_functions.read_from_file("CBSD_Project_Flask/JSON_Files/retrieve_quote.json") #Read back from the file
 
-def aqi_parameter():
-    url = "https://api.airvisual.com/v2/nearest_city?key="
-    aqi_key = main_functions.read_from_file("AQI_WebApp_Flask/JSON_Files/aqi_key.json")
-    aqi_key = aqi_key['aqi_key']
+    #Parse to dictionary
+    params = {"companyName":  response_json_file["companyName"],
+              "latestPrice" : response_json_file["latestPrice"]}
 
-    url2 = url+aqi_key
+    return params
 
-    request_json = requests.get(url2).json()
-
-    main_functions.save_to_file(request_json, "AQI_WebApp_Flask/JSON_Files/aqi.json")
-
-    air_quality_index = main_functions.read_from_file("AQI_WebApp_Flask/JSON_Files/aqi.json")
-
-    '''
-    From the json file read above, please find the right values for the variables below
-    Notice that I already accessed the values for latitude and longitude
-    and I also concatenated them into one variable called coordinates
-    '''
-
-    latitude = air_quality_index['data']['location']['coordinates'][0]
-    longitude = air_quality_index['data']['location']['coordinates'][1]
-    coordinates = str(latitude)+', '+str(longitude)
-
-    temperatureC = air_quality_index['data']['current']['weather']['tp']
-    pressure = air_quality_index['data']['current']['weather']['pr']
-    humidity = air_quality_index['data']['current']['weather']['hu']
-    aqius = air_quality_index['data']['current']['pollution']['aqius']
-
-    parameters = {'coordinates': coordinates,
-                  'temperatureC': str(temperatureC),
-                  'pressure': str(pressure),
-                  'humidity': str(humidity),
-                  'aqius': str(aqius)}
-    return parameters
 
