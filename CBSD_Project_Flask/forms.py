3e6265f232ff43118ab77e93e68c6c7a4bc524c5
Juandiego Rodriguez <jdrod0803@gmail.com>
2020-03-31 15:23:09 -0400

    Finished quote params, company params.

--- CBSD_Project_Flask/forms.py
+++ CBSD_Project_Flask/forms.py
@@ -22,8 +22,24 @@ def retrieve_quote_parameters(symbol):
 
     #Parse to dictionary
     params = {"companyName":  response_json_file["companyName"],
-              "latestPrice" : response_json_file["latestPrice"]}
+              "latestPrice" : response_json_file["latestPrice"],
+              "avgTotalVolume": response_json_file["avgTotalVolume"],
+              "marketCap": response_json_file["marketCap"],
+              "week52High": response_json_file["week52High"],
+              "week52Low": response_json_file["ytdChange"]}
 
     return params
 
+def retreive_company_parameters(symbol):
+    base_url = "https://cloud.iexapis.com/stable/stock/" + symbol + "/company"
+    iex_key = main_functions.read_from_file("CBSD_Project_Flask/JSON_Files/iex_key.json")["iex_key"]
+    request_url = base_url + "?token=" + iex_key
+    #JSON Stuff
+    response_json = requests.get(request_url).json() #Get the json based on the URL
+    main_functions.save_to_file(response_json, "CBSD_Project_Flask/JSON_Files/retrieve_company.json") #Save the json to file
+    response_json_file = main_functions.read_from_file("CBSD_Project_Flask/JSON_Files/retrieve_company.json") #Read back from the file
 
+    params = {"industry": response_json_file["industry"],
+              "website": response_json_file["website"],
+              "description":response_json_file["description"]}
+    return params
